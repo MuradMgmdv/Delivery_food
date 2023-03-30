ab0e27d0654e461360f443ef203d3a3f9dd08cd6
Murad <murad186992@mail.ru>
2023-03-30 11:46:20 +0300

    get_items

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,16 @@ import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
-import pizzas from './assets/pizzas.json'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios('https://62f4c3c7535c0c50e761b9aa.mockapi.io/items').then((res) => setItems(res.data));
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -17,12 +24,16 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {
-              pizzas.map((el, i) => (
-                <PizzaBlock key={el.id} title={el.title} price={el.price} imageUrl={el.imageUrl} sizes={el.sizes} types={el.types}/>
-              ))
-            }
-          
+            {items.map((el, i) => (
+              <PizzaBlock
+                key={el.id}
+                title={el.title}
+                price={el.price}
+                imageUrl={el.imageUrl}
+                sizes={el.sizes}
+                types={el.types}
+              />
+            ))}
           </div>
         </div>
       </div>
