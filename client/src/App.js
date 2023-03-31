775dbdb205616d0d5168c88b6911efcf748095cd
Murad <murad186992@mail.ru>
2023-03-31 13:44:21 +0300

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import './scss/app.scss';
 import Header from './components/Header';
 import Home from './pages/Home';
+import { Routes, Route } from 'react-router-dom';
 import NotFound from './pages/NotFound';
+import Cart from './pages/Cart';
 
 function App() {
   return (
@@ -9,8 +11,11 @@ function App() {
       <Header />
       <div className="content">
         <div className="container">
-        {/* <Home /> */}
-        <NotFound />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,20 @@
+import { Link } from 'react-router-dom';
+
 function Header() {
-    return (
-      <div className="header">
+  return (
+    <div className="header">
       <div className="container">
-        <div className="header__logo">
-          <img width="38" src="./img/pizza-logo.svg" alt="Pizza logo" />
-          <div>
-            <h1>Delivery food</h1>
-            <p>самая вкусная пицца</p>
+        <Link to="/">
+          <div className="header__logo">
+            <img width="38" src="./img/pizza-logo.svg" alt="Pizza logo" />
+            <div>
+              <h1>Delivery food</h1>
+              <p>самая вкусная пицца</p>
+            </div>
           </div>
-        </div>
+        </Link>
         <div className="header__cart">
-          <a href="/cart.html" className="button button--cart">
+          <Link to="/cart" className="button button--cart">
             <span>520 ₽</span>
             <div className="button__delimiter"></div>
             <svg
@@ -42,11 +46,11 @@ function Header() {
               />
             </svg>
             <span>3</span>
-          </a>
+          </Link>
         </div>
       </div>
     </div>
-    )
-  }
+  );
+}
 
-  export default Header
+export default Header;
