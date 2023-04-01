88e7c02605277c988402feb6b734c09ec42e9192
Murad <murad186992@mail.ru>
2023-04-01 11:24:40 +0300

    @media

--- client/src/components/PizzaBlock/PizzaBlock.jsx
+++ client/src/components/PizzaBlock/PizzaBlock.jsx
@@ -7,7 +7,8 @@ function PizzaBlock({ title, price, imageUrl, sizes, types }) {
   const typeNames = ['тонкое', 'традиционное'];
 
   return (
-    <div className="pizza-block">
+   <div className="pizza-block-wrapper">
+     <div className="pizza-block">
       <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
       <h4 className="pizza-block__title">{title}</h4>
       <div className="pizza-block__selector">
@@ -51,6 +52,7 @@ function PizzaBlock({ title, price, imageUrl, sizes, types }) {
         </button>
       </div>
     </div>
+   </div>
   );
 }
 
