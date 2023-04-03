d807b0dfea4d5e70056fc1de4964c608ded6d62d
Murad <murad186992@mail.ru>
2023-04-03 14:34:45 +0300

    styles

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -14,6 +14,27 @@ const Search = ({ searchValue, setSearchValue }) => {
           fill="#000000"
         />
       </svg>
+      <svg className={styles.container__clear}
+        width="800px"
+        height="800px"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M8 8L16 16"
+          stroke="#000000"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M16 8L8 16"
+          stroke="#000000"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
       <input
         value={searchValue}
         onChange={(e) => setSearchValue(e.target.value)}
