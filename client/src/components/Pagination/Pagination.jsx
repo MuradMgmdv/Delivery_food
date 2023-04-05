e689a491ee1a94a6c85178329dbe34266dedefc5
Murad <murad186992@mail.ru>
2023-04-05 11:42:37 +0300

    styles_pagination

--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactPaginate from 'react-paginate';
+import styles from './styles.module.scss'
 
 const Pagination = ({ onChangePage }) => {
   return (
-    <div>
+    <div className={styles.root} >
       <ReactPaginate
         nextLabel=">"
         onPageChange={(event) => onChangePage(event.selected + 1)}
