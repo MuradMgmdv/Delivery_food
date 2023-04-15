2602a368ade8743a28bcb1ef7530f1f27bee7961
Murad <murad186992@mail.ru>
2023-04-15 16:00:01 +0300

    outlet_dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import './scss/app.scss';
-import Header from './components/Header';
 import Home from './pages/Home';
 import { Routes, Route } from 'react-router-dom';
 import NotFound from './pages/NotFound';
 import Cart from './pages/Cart';
 import FullPizza from './pages/FullPizza';
+import MainLayouts from './layouts/MainLayouts';
 
 function App() {
   return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/pizza/:id" element={<FullPizza />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </div>
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<MainLayouts />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/pizza/:id" element={<FullPizza />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ client/src/layouts/MainLayouts.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from '../components/Header';
+
+const MainLayouts = () => {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="container content">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default MainLayouts;
