0ec36dcafd35b41d66a32acd90ca53c7e70e04b4
Murad <murad186992@mail.ru>
2023-04-18 14:32:42 +0300

    ts_props

--- client/src/components/Categories.tsx
+++ client/src/components/Categories.tsx
@@ -1,4 +1,9 @@
-function Categories({ value, onClickCategory }) {
+type CategoriesProps = {
+  value: number;
+  onClickCategory: any;
+};
+
+const Categories: React.FC<CategoriesProps> = ({ value, onClickCategory }) => {
   const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
 
   return (
@@ -12,6 +17,6 @@ function Categories({ value, onClickCategory }) {
       </ul>
     </div>
   );
-}
+};
 
 export default Categories;
